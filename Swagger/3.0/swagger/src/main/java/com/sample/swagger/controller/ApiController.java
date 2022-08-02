af76c01585d6dceaaf61754818474d95a9157f81
Chiptune93 <eoen012@gmail.com>
2022-08-02 11:54:29 +0900

    sample update

--- Swagger/3.0/swagger/src/main/java/com/sample/swagger/controller/ApiController.java
+++ Swagger/3.0/swagger/src/main/java/com/sample/swagger/controller/ApiController.java
@@ -5,22 +5,29 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sample.swagger.data.addUserRes;
 import com.sample.swagger.data.data;
 import com.sample.swagger.data.dataParam;
-import com.sample.swagger.data.dataRes;
+import com.sample.swagger.data.deleteUserRes;
+import com.sample.swagger.data.getUserRes;
+import com.sample.swagger.data.updateUserRes;
 import com.sample.swagger.response.ApiErr;
 import com.sample.swagger.response.ApiNoAuth;
 import com.sample.swagger.response.ApiRes;
 
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.Authorization;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -33,30 +40,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 public class ApiController {
 
     @Tag(name = "user")
-    @ApiOperation(value = "유저 리스트", notes = "파라미터로 넘어온 수 만큼 유저를 리턴한다.", authorizations = {@Authorization(value = "apiKey") })
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "요청 성공", content = @Content(schema = @Schema(implementation = ApiRes.class))),
-            @ApiResponse(responseCode = "201", description = "500과 동일"),
-            @ApiResponse(responseCode = "401", description = "권한 없음(키누락)", content = @Content(schema = @Schema(implementation = ApiNoAuth.class))),
-            @ApiResponse(responseCode = "403", description = "500과 동일"),
-            @ApiResponse(responseCode = "404", description = "500과 동일"),
-            @ApiResponse(responseCode = "500", description = "요청 실패", content = @Content(schema = @Schema(implementation = ApiErr.class)))
-    })
-    @GetMapping(value = "/getUser", produces = "application/json")
-    public ResponseEntity<ApiRes<dataRes>> getUser(@RequestParam int num) {
-        dataRes res = new dataRes();
-        List<data> result = new ArrayList<data>();
-        for (int i = 0; i < num; i++) {
-            data d = new data();
-            d.setUserNo(i);
-            d.setUsername("user" + i);
-            result.add(d);
-        }
-        res.setDataList(result);
-        return new ResponseEntity<ApiRes<dataRes>>(new ApiRes<dataRes>(res), HttpStatus.OK);
-    }
-
-    @Tag(name = "user")
     @ApiOperation(value = "유저 리스트", notes = "파라미터로 넘어온 수 만큼 유저를 리턴한다.", authorizations = {
             @Authorization(value = "apiKey") })
     @ApiResponses(value = {
@@ -67,17 +50,75 @@ public class ApiController {
             @ApiResponse(responseCode = "404", description = "500과 동일"),
             @ApiResponse(responseCode = "500", description = "요청 실패", content = @Content(schema = @Schema(implementation = ApiErr.class)))
     })
-    @PostMapping(value = "/getUser", produces = "application/json")
-    public ResponseEntity<ApiRes<dataRes>> postUser(@RequestBody dataParam param) {
-        dataRes res = new dataRes();
+    @GetMapping(value = "/{version}/getUser", produces = "application/json")
+    public ResponseEntity<ApiRes<getUserRes>> getUser(@RequestParam int num,
+            @Parameter(name = "version", description = "API 버전을 입력하세요. ", example = "V1", required = true) @PathVariable String version) {
+        getUserRes res = new getUserRes();
         List<data> result = new ArrayList<data>();
-        for (int i = 0; i < param.getNum(); i++) {
+        for (int i = 0; i < num; i++) {
             data d = new data();
             d.setUserNo(i);
             d.setUsername("user" + i);
             result.add(d);
         }
         res.setDataList(result);
-        return new ResponseEntity<ApiRes<dataRes>>(new ApiRes<dataRes>(res), HttpStatus.OK);
+        return new ResponseEntity<ApiRes<getUserRes>>(new ApiRes<getUserRes>(res), HttpStatus.OK);
+    }
+
+    @Tag(name = "user")
+    @ApiOperation(value = "유저 추가", notes = "새로운 사용자를 추가한다.", authorizations = {
+            @Authorization(value = "apiKey") })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청 성공", content = @Content(schema = @Schema(implementation = ApiRes.class))),
+            @ApiResponse(responseCode = "201", description = "500과 동일"),
+            @ApiResponse(responseCode = "401", description = "권한 없음(키누락)", content = @Content(schema = @Schema(implementation = ApiNoAuth.class))),
+            @ApiResponse(responseCode = "403", description = "500과 동일"),
+            @ApiResponse(responseCode = "404", description = "500과 동일"),
+            @ApiResponse(responseCode = "500", description = "요청 실패", content = @Content(schema = @Schema(implementation = ApiErr.class)))
+    })
+    @PostMapping(value = "/{version}/addUser", produces = "application/json")
+    public ResponseEntity<ApiRes<addUserRes>> addUser(@RequestBody data param,
+            @Parameter(name = "version", description = "API 버전을 입력하세요. ", example = "V1", required = true) @PathVariable String version) {
+        addUserRes res = new addUserRes();
+        res.setUserNo(param.getUserNo());
+        return new ResponseEntity<ApiRes<addUserRes>>(new ApiRes<addUserRes>(res), HttpStatus.OK);
+    }
+
+    @Tag(name = "user")
+    @ApiOperation(value = "유저 업데이트", notes = "사용자를 업데이트 한다.", authorizations = {
+            @Authorization(value = "apiKey") })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청 성공", content = @Content(schema = @Schema(implementation = ApiRes.class))),
+            @ApiResponse(responseCode = "201", description = "500과 동일"),
+            @ApiResponse(responseCode = "401", description = "권한 없음(키누락)", content = @Content(schema = @Schema(implementation = ApiNoAuth.class))),
+            @ApiResponse(responseCode = "403", description = "500과 동일"),
+            @ApiResponse(responseCode = "404", description = "500과 동일"),
+            @ApiResponse(responseCode = "500", description = "요청 실패", content = @Content(schema = @Schema(implementation = ApiErr.class)))
+    })
+    @PutMapping(value = "/{version}/updateUser", produces = "application/json")
+    public ResponseEntity<ApiRes<updateUserRes>> updateUser(@RequestBody data param,
+            @Parameter(name = "version", description = "API 버전을 입력하세요. ", example = "V1", required = true) @PathVariable String version) {
+        updateUserRes res = new updateUserRes();
+        res.setUserNo(param.getUserNo());
+        return new ResponseEntity<ApiRes<updateUserRes>>(new ApiRes<updateUserRes>(res), HttpStatus.OK);
+    }
+
+    @Tag(name = "user")
+    @ApiOperation(value = "유저 삭제", notes = "파라미터로 넘어온 데이터의 유저를 삭제한다.", authorizations = {
+            @Authorization(value = "apiKey") })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요청 성공", content = @Content(schema = @Schema(implementation = ApiRes.class))),
+            @ApiResponse(responseCode = "201", description = "500과 동일"),
+            @ApiResponse(responseCode = "401", description = "권한 없음(키누락)", content = @Content(schema = @Schema(implementation = ApiNoAuth.class))),
+            @ApiResponse(responseCode = "403", description = "500과 동일"),
+            @ApiResponse(responseCode = "404", description = "500과 동일"),
+            @ApiResponse(responseCode = "500", description = "요청 실패", content = @Content(schema = @Schema(implementation = ApiErr.class)))
+    })
+    @DeleteMapping(value = "/{version}/deleteUser", produces = "application/json")
+    public ResponseEntity<ApiRes<deleteUserRes>> deleteUser(@RequestBody data param,
+            @Parameter(name = "version", description = "API 버전을 입력하세요. ", example = "V1", required = true) @PathVariable String version) {
+        deleteUserRes res = new deleteUserRes();
+        res.setUserNo(param.getUserNo());
+        return new ResponseEntity<ApiRes<deleteUserRes>>(new ApiRes<deleteUserRes>(res), HttpStatus.OK);
     }
 }
--- Swagger/3.0/swagger/src/main/java/com/sample/swagger/data/getUserRes.java
+++ Swagger/3.0/swagger/src/main/java/com/sample/swagger/data/getUserRes.java
@@ -6,10 +6,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
-@Schema(name = "dataRes", description = "유저 조회 응답 데이터")
+@Schema(name = "getUserRes", description = "유저 조회 응답 데이터")
 @Getter
 @Setter
-public class dataRes {
+public class getUserRes {
     @Schema(description = "유저 리스트", defaultValue = "", allowableValues = {}, example = "")
     private List<data> dataList;
 }
