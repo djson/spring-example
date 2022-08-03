841ad3ad00f608ccb77e3443104fd15f2aff23ee
Chiptune93 <eoen012@gmail.com>
2022-08-03 10:39:41 +0900

    H2 Database Sample Commit

---
+++ H2 Database/sample/src/test/java/springboot/h2/sample/SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.h2.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
